d4ff23fe1e2c7ab57756ded1c1a0e9bb46352d0c
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-07-10 01:31:44 -0400

    Removed comments

--- client.py
+++ client.py
@@ -11,11 +11,3 @@ class Client:
 
     def __repr__(self):
         return f"self.name = {self.name} : self.question = {self.question}"
-
-    # def save_reading(self, reading):
-        # save the reading from spread.py for client to access later
-        # Will include the question, the spread, and the cards
-    def reading(self, card_name):
-        # super().__init__(card_name, card_description, key_words)
-        # return f"{card_name}:\n{card_description}\n{key_words}"
-        pass
