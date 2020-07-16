8c7c12c95c9072cb21f8dca9f5b38c6b9ec30675
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-07-15 23:51:58 -0400

    Added all the basics for the major arcana

--- major_arcana.py
+++ major_arcana.py
@@ -12,7 +12,7 @@ the_magician = Card("1 - The Magician",
 This might be in your new job, new business venture, or possibly a new love interest.
 Holding back would only mean missing the opportunity of becoming the best version of yourself.
 Depending on your choice, there will be change ahead, so have faith in yourself.""", 
-["manifestation", "resourcefullness", "power"])
+["manifestation", "resourcefulness", "power"])
 
 the_high_priestess = Card("2 - The High Priestess", 
 """This is a time to search within yourself. As hard as you try to find answers, 
@@ -28,15 +28,77 @@ Due to the likely successful outcome, this is the time to devote yourself toward
 
 the_emperor = Card("4 - The Emperor", 
 """This card symbolizes control, authority, organization and fatherhood. 
-This is someone that guides with a firm hand, gives structure, creates rules and imparts knwledge.
+This is someone that guides with a firm hand, gives structure, creates rules and imparts knowledge.
 His goals are always aligned with the greater good.""", 
 ["structure", "authority", "discipline"])
 
-the_hierophant = Card("0 - The Hierophant", "This is the Hierophant's description", ["beginnings", "starts"])
-the_lovers = Card("0 - The Lovers", "This is the Lover's description", ["beginnings", "starts"])
-the_chariot = Card("0 - The Chariot", "This is the Chariot's description", ["beginnings", "starts"])
-strength = Card("0 - Strength", "This is Strength's description", ["beginnings", "starts"])
-the_hermit = Card("0 - The Hermit", "This is the Hermit's description", ["beginnings", "starts"])
+the_hierophant = Card("5 - The Hierophant", 
+"""This is the Hierophant's description""", 
+["beginnings", "starts"])
+
+the_lovers = Card("6 - The Lovers", 
+"""This is the Lover's description""", 
+["beginnings", "starts"])
+
+the_chariot = Card("7 - The Chariot", 
+"""This is the Chariot's description""", 
+["beginnings", "starts"])
+
+strength = Card("8 - Strength", 
+"""This is Strength's description""", 
+["beginnings", "starts"])
+
+the_hermit = Card("9 - The Hermit", 
+"""This is the Hermit's description""", 
+["beginnings", "starts"])
+
+wheel_of_fortune = Card("10 - Wheel of Fortune", 
+"""This is the Wheel of Fortune's description""", 
+["beginnings", "starts"])
+
+justice = Card("11 - Justice", 
+"""This is the Justice's description""", 
+["beginnings", "starts"])
+
+the_hanged_man = Card("12 - The Hanged Man", 
+"""This is the Hanged Man's description""", 
+["beginnings", "starts"])
+
+death = Card("13 - Death", 
+"""This is the Death's description""", 
+["beginnings", "starts"])
+
+temperance = Card("14 - Temperance", 
+"""This is Temperance's description""", 
+["beginnings", "starts"])
+
+the_devil = Card("15 - The Devil", 
+"""This is the Devil's description""", 
+["beginnings", "starts"])
+
+the_tower = Card("16 - The Tower", 
+"""This is the Tower's description""", 
+["beginnings", "starts"])
+
+the_star = Card("17 - The Star", 
+"""This is the Star's description""", 
+["beginnings", "starts"])
+
+the_moon = Card("18 - The Moon", 
+"""This is the Moon's description""", 
+["beginnings", "starts"])
+
+the_sun = Card("19 - The Sun", 
+"""This is the Sun's description""", 
+["beginnings", "starts"])
+
+judgement = Card("20 - Judgement", 
+"""This is Judgement's description""", 
+["beginnings", "starts"])
+
+the_world = Card("21 - The World", 
+"""This is the World's description""", 
+["beginnings", "starts"])
 
 tarot_card_names = {
   1: the_fool,
@@ -48,5 +110,17 @@ tarot_card_names = {
   7: the_lovers,
   8: the_chariot,
   9: strength,
-  10: the_hermit
+  10: the_hermit,
+  11: wheel_of_fortune,
+  12: justice,
+  13: the_hanged_man,
+  14: death,
+  15: temperance,
+  16: the_devil,
+  17: the_tower,
+  18: the_star,
+  19: the_moon,
+  20: the_sun,
+  21: judgement,
+  22: the_world
 }
